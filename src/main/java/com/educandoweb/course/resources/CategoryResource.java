892318b900df6276f00a279465f135c9740bbccb
Pedro Henrique <pedrohenrique.santos.dev@gmail.com>
2026-08-31 18:13:45 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/resources/CategoryResource.java
+++ src/main/java/com/educandoweb/course/resources/CategoryResource.java
@@ -4,6 +4,7 @@ import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +18,13 @@ public class CategoryResource {
     @Autowired
     private CategoryService service;
 
-    @RequestMapping
+    @GetMapping
     public ResponseEntity<List<Category>> findAll() {
         List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
-    @RequestMapping(value = "/{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Category> findById(@PathVariable Long id) {
         Category obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
